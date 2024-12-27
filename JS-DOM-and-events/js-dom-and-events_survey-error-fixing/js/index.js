ac3654f6468c1987c5dc05fa88e475a48283c1fc
Sara Zmak <zmaksara@gmail.com>
2024-12-27 12:11:50 +0100

    add missing log to console

--- JS-DOM-and-events/js-dom-and-events_survey-error-fixing/js/index.js
+++ JS-DOM-and-events/js-dom-and-events_survey-error-fixing/js/index.js
@@ -25,7 +25,7 @@ const secondChoiceButton = document.querySelector(
 firstChoiceButton.addEventListener("click", () => {
   firstChoiceButton.classList.add("active-choice");
   secondChoiceButton.classList.remove("active-choice");
-  console("Selected first choice");
+  console.log("Selected first choice");
 });
 
 // 4. When clicking on the second choice, the button should be shown with an active state. Do you see why it isn't working?
