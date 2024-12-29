0c1659a45ad7d8ebb25f84c5b1ce488e469e2311
Sara Zmak <zmaksara@gmail.com>
2024-12-29 17:04:45 +0100

    weather check part 1

--- js-conditions-and-booleans/js-conditions-and-booleans_weather-check/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_weather-check/js/index.js
@@ -1,5 +1,19 @@
 // Change this value to test different "weather" conditions.
-const weather = "rainy"
+console.log("What is the weather like today?");
+const weather = "sunny";
 
+switch (weather) {
+  case "sunny":
+    console.log("The sun is shining, the weather is sweet!");
+    break;
+  case "rainy":
+    console.log("It's raining men, hallelujah!");
+    break;
+  case "snowy":
+    console.log("Let it snow, let it snow, let it snow!");
+    break;
+  default:
+    console.log("Sorry, I don't know what the weather is like today!");
+}
 // Adjust the "temperature" value to trigger different messages (for Part 2 only).
-const temperature = 22; 
+const temperature = 22;
