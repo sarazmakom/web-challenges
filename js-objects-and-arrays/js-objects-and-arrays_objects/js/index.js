9b679b1ff6919c0c4ce0d7686991ae72fcbfb593
Sara Zmak <zmaksara@gmail.com>
2024-12-29 20:04:34 +0100

    fix ex 2 and ex 3

--- js-objects-and-arrays/js-objects-and-arrays_objects/js/index.js
+++ js-objects-and-arrays/js-objects-and-arrays_objects/js/index.js
@@ -17,8 +17,8 @@ const personAlex = {
   age: 24,
 };
 
-const nameOfAlex = "Change me";
-const ageOfAlex = "Change me";
+const nameOfAlex = personAlex.name;
+const ageOfAlex = personAlex.age;
 
 // EXERCISE 3
 // Change the `name` to "Alex" and `age` to "35" by updating the `personToChange`object: `person.existingProperty = newValue`.
@@ -27,6 +27,9 @@ const personToChange = {
   name: "Kim",
   age: 24,
 };
+
+  personToChange.name = "Alex";
+  personToChange.age = "35";
 
 // EXERCISE 4
 // Add the properties `age` with value "5" and `breed` with value "husky" to the `petPluto` object: `pet.newProperty = newValue`.
