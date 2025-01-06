f2a705db3fafd164daeaa6633ff93114ce331374
Sara Zmak <zmaksara@gmail.com>
2025-01-06 01:25:01 +0100

    js cleateElement toast messages

--- js-createelement/js-createelement_toast-messages/js/index.js
+++ js-createelement/js-createelement_toast-messages/js/index.js
@@ -6,8 +6,13 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const newToast = document.createElement("li");
+  newToast.classList.add("toast-container__message");
+  newToast.textContent = "I'm a new toast message.";
+  toastContainer.appendChild(newToast);
 });
 
 clearButton.addEventListener("click", () => {
+  toastContainer.innerHTML = "";
   // Exercise: Clear the stack of toast messages
 });
