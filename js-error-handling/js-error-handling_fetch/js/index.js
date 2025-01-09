53e4c9323adad3b8a942ca45b475e027be79eae1
Sara Zmak <zmaksara@gmail.com>
2025-01-09 13:16:43 +0100

    js error handling fetch

--- js-error-handling/js-error-handling_fetch/js/index.js
+++ js-error-handling/js-error-handling_fetch/js/index.js
@@ -7,7 +7,9 @@ const errorElement = document.querySelector("[data-js='error']");
 async function fetchUserData(url) {
   try {
     const response = await fetch(url);
-
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: User nowhere to be found.`);
+    }
     return await response.json();
   } catch (error) {
     return { error: error.message };
