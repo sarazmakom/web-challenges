c01d7f7d2377bd5aca9f5d16473efe79188f1e70
Sara Zmak <zmaksara@gmail.com>
2025-01-09 11:36:35 +0100

    js error handling calculator

--- js-error-handling/js-error-handling_calculator/js/index.js
+++ js-error-handling/js-error-handling_calculator/js/index.js
@@ -2,6 +2,7 @@ console.clear();
 
 const form = document.querySelector("form");
 const output = form.querySelector("output");
+const errorOutput = document.querySelector(".error");
 
 const operations = {
   add: (a, b) => a + b,
@@ -9,7 +10,7 @@ const operations = {
   multiply: (a, b) => a * b,
   divide: (a, b) => {
     if (b === 0) {
-      throw new Error("Cannot divide by zero!");
+      throw new Error("Cannot divide by zero! Try again!");
     }
     return a / b;
   },
@@ -17,8 +18,16 @@ const operations = {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
   const firstNumber = Number(event.target.firstNumber.value);
   const secondNumber = Number(event.target.secondNumber.value);
   const operation = event.target.operation.value;
-  output.innerText = operations[operation](firstNumber, secondNumber);
+
+  try {
+    errorOutput.innerText = "";
+    output.innerText = operations[operation](firstNumber, secondNumber);
+  } catch (error) {
+    output.innerText = "";
+    errorOutput.innerText = error.message;
+  }
 });
